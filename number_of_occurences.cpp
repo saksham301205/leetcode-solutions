72f1ce19840f9ee2ac9d5728bc913adca4c8e771
Saksham Begad <skmbegad@gmail.com>
2026-06-16 16:47:07 +0530

    Create number_of_occurences.cpp

---
+++ number_of_occurences.cpp
@@ -0,0 +1,54 @@
+int firstOccurrence(vector<int>& arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int first = -1;
+
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if(arr[mid] == x) {
+            first = mid;
+            high = mid - 1;
+        }
+        else if(arr[mid] < x) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    return first;
+}
+
+int lastOccurrence(vector<int>& arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int last = -1;
+
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if(arr[mid] == x) {
+            last = mid;
+            low = mid + 1;
+        }
+        else if(arr[mid] < x) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    return last;
+}
+
+int count(vector<int>& arr, int n, int x) {
+    int first = firstOccurrence(arr, n, x);
+
+    if(first == -1)
+        return 0;
+
+    int last = lastOccurrence(arr, n, x);
+
+    return last - first + 1;
+}
