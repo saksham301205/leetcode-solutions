8880aa8120950302f58ca88233421aeb5c29274b
Saksham Begad <skmbegad@gmail.com>
2026-06-23 17:16:26 +0530

    Update Sqrt(X).cpp

--- Sqrt(X).cpp
+++ Sqrt(X).cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    long long binarySearch(int n){
-        int s = 0;
-        int e = n;
-        long long mid = s + (e-s)/2;
-        int ans = -1;
-        while(s<=e){
-            long long square = mid*mid;
-            if(square == n){
-                return mid;
-            }
-            else if(square <= n){
-                ans = mid;
-                s = mid + 1;
-            }
-            else {
-                e = mid - 1;
-            }
-            mid = s + (e-s)/2;
-        }
-        return ans;
-    }
     int mySqrt(int x) {
-        return binarySearch(x);
-    }   
+        long long low = 1, high = x;
+
+        while(low <= high){
+            long long mid = (low + high) / 2;
+            long long val = mid * mid;
+
+            if(val <= x)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return high;
+    }
 };
